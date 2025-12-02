3c18e0fcac122010ee297f7c6e2adb6d244e528f
colan1ch <nachernev@gmail.com>
2025-12-02 13:07:14 +0300

    prst

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "0dcbc004255934090c10f1fb67d31c7c"
   }, {
     "url": "index.html",
-    "revision": "0.di9r5bhn4i"
+    "revision": "0.f0msl67kj8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
