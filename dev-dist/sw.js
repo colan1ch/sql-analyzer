56068bed4a6c168e323bcfc0bf0ebd8f0bf66579
colan1ch <nachernev@gmail.com>
2025-12-15 20:54:28 +0300

    192 + homepage

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "0dcbc004255934090c10f1fb67d31c7c"
   }, {
     "url": "index.html",
-    "revision": "0.f0msl67kj8"
+    "revision": "0.3u72uk0vvao"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
